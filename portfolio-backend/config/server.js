1dd6542ba4c32604ece94af238ec919e5a30cb73
Miriam Dong <miriamdong@gmail.com>
2021-05-06 18:00:19 -0700

    add backend

---
+++ portfolio-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '771825be6451c340597f9dbe22ae9f96'),
+    },
+  },
+});
---
+++ portfolio-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '22edacc8-7cc4-49e4-968e-ac643e0d9f03'
+};
